7aaac57ebcb716f46e3f2e70c8bec839ba4d17f1
maria eduarda <mariaeduardadebritto12345@gmail.com>
2024-09-09 18:43:17 -0300

    2 - g.c

--- 2 - g.c
+++ 2 - g.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(){
+
+    float pressao;
+    float volume;
+    float temperatura;
+    float massa_ar;
+
+    printf("bem vindo ao software para calculo da massa de ar de um pneu! \n\n");
+
+    printf("informe a pressao do pneu: ");
+    scanf("%f", &pressao);
+
+    printf("\ninforme o volume do pneu: ");
+    scanf("%f", &volume);
+
+    printf("\ninforme a temperatura do pneu: ");
+    scanf("%f", &temperatura);
+
+    massa_ar = ((pressao*volume)/(0.37*(temperatura+460)));
+
+    printf("A massa de ar do pneu sera: %.3f", massa_ar);
+
+    return 0;
+
+}
