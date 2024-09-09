588430059a7044fe0b4bb939abe5a20cb0f632b7
maria eduarda <mariaeduardadebritto12345@gmail.com>
2024-09-09 18:44:35 -0300

    2 - h.c

--- 2 - h.c
+++ 2 - h.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+int main(){
+
+    float a1;
+    float r;
+    float an;
+    int n;
+
+    printf("\tProgressao Aritmetica - PA\n");
+
+    printf("\nEntre com o valor do primeiro termo: ");
+    scanf("%f", &a1);
+
+    printf("\nEntre com o valor da razao: ");
+    scanf("%f", &r);
+
+    printf("\nEntre com o numero dos termos: ");
+    scanf("%d", &n);
+
+    an = (a1+(n - 1) * r);
+
+    printf("\n\nE-nesimo termo da PA - an: %f\n\n", an);
+
+    return 0;
+}
