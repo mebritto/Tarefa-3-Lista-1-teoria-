9df1f22e8de06f93560d40fa1e189d4a35611a6a
maria eduarda <mariaeduardadebritto12345@gmail.com>
2024-09-09 18:28:59 -0300

    2 -  j.c

--- 2 -  j.c
+++ 2 -  j.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int notas_1_real;
+    int notas_10_reais;
+    int notas_50_reais;
+    int notas_100_reais;
+    int total;
+
+    printf("digite a quantidade de notas de 1 real: ");
+    scanf("%d", &notas_1_real);
+
+    printf("\ndigite a quantidade de notas de 10 reais: ");
+    scanf("%d", &notas_10_reais);
+
+    printf("\ndigite a quantidade de notas de 50 reais: ");
+    scanf("%d", &notas_50_reais);
+
+    printf("\ndigite a quantidade de notas de 100 reais: ");
+    scanf("%d", &notas_100_reais);
+
+    total = (notas_1_real * 1) + (notas_10_reais * 10) + (notas_50_reais * 50) + (notas_100_reais * 100);
+
+    printf("O valor total é R$%d\n", total);
+
+    return 0;
+}
 
