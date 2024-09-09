f6d272523af7beffaaca1947baf95d3bd27297ad
maria eduarda <mariaeduardadebritto12345@gmail.com>
2024-09-09 18:39:55 -0300

    2 - d.c

--- 2 - d.c
+++ 2 - d.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+
+    float raio;
+    float area;
+    float pi;
+    float valor;
+
+    pi = 3.14;
+
+    printf("digite o raio do seu circulo: \n");
+    scanf("%f", &raio);
+
+
+    valor = raio*raio;
+    area = valor*pi;
+
+    printf("a area do seu circulo sera: %f", area);
+
+return 0;
+}
 
