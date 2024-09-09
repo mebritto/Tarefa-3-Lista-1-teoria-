edaebec343bd67a0b85c3f64b3802def13c09544
maria eduarda <mariaeduardadebritto12345@gmail.com>
2024-09-09 17:21:59 -0300

    Add files via upload

---
+++ 1 - a.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+    long long int n = 4000000000;
+    printf ("%lld\n",n);
+
+return 0;
+}
---
+++ 1 - b.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+    int a;
+
+    printf("%d\n", a);
+
+return 0;
+}
