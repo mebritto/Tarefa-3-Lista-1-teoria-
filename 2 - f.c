d885c3942b9ea6e55d74cdc4e2840d8d25d58b97
maria eduarda <mariaeduardadebritto12345@gmail.com>
2024-09-09 18:42:03 -0300

    2 - f.c

--- 2 - f.c
+++ 2 - f.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(){
+
+    float temp_f;
+
+    printf("seja bem vindo ao nosso programa de conversao de temperatura!\n\n");
+    printf("digite a temperatura em graus farenheit: \n");
+    scanf("%f", &temp_f);
+
+    float temp_c = 0.5555*(temp_f-32);
+
+    printf("o resultado em graus celcius sera: %f", temp_c);
+
+    return 0;
+}
